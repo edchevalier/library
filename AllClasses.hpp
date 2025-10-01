3ef7b518caca17d469256883e0b64551585c5fbb
edchevalier <edouard.chevalier@tudent.junia.com>
2025-10-01 20:29:34 +0200

    Réorganisation des fichiers

--- AllClasses.hpp
+++ AllClasses.hpp
@@ -1,8 +1,21 @@
 #pragma once
 
-// Inclut toutes les classes du projet
-#include "Resource.hpp"
-#include "Book.hpp"
-#include "Video.hpp"
+// Inclut toutes les classes de ressources du projet
+#include "Ressources/Resource.hpp"
+#include "Ressources/Book.hpp"
+#include "Ressources/Video.hpp"
+#include "Ressources/CD.hpp"
+#include "Ressources/DVD.hpp"
+#include "Ressources/VHS.hpp"
+#include "Ressources/Digital.hpp"
+#include "Ressources/Reviews.hpp"
 
-// Ajoute ici d'autres classes si nécessaire
+// Inclut les autres classes d'utilisateurs du projet
+#include "Users/User.hpp"
+#include "Users/Admin.hpp"
+#include "Users/Client.hpp"
+
+// Inclut les autres classes nécessaires
+#include "mainClasses/Library.hpp"
+#include "mainClasses/Session.hpp"
+#include "mainClasses/FileManager.hpp"
